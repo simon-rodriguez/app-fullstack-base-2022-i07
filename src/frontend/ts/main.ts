33cde87f9437583ece70ee32fa270babac866d80
Simon <95500626+simon-rodriguez@users.noreply.github.com>
2022-10-31 19:19:51 -0300

    Cambio en Boton (Clase 2/8)

--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -1,6 +1,18 @@
+window.onload = inicio;
 
-function SayHello(){
+function inicio(){
+    let btn = document.getElementById("btnSaludar");
+
+    btn.onclick = saludar;
+}
+
+function saludar() {
+    alert("Hola");
+}
+
+/* function SayHello(){
     let current_value = document.getElementById("textarea_1") as HTMLInputElement;
     let new_value = "Hello world!!!" + "\n" + current_value.value;
     document.getElementById("textarea_1").innerHTML = new_value;
 }
+*/  
